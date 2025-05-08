806957232db0cc24151e5cd02e30feb8d5a1a701
SandaraD <sandara6877@gmail.com>
2025-05-08 19:36:36 +0530

    Improved auth persistence handling

--- src/App.js
+++ src/App.js
@@ -6,44 +6,52 @@ import FavoritesPage from './pages/FavoritesPage';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import NavBar from './components/NavBar';
+import AuthBackground from './components/AuthBackground';
 import './styles/App.css';
 import { auth } from './firebase';
-import AuthBackground from './components/AuthBackground';
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const unSubscribe = auth.onAuthStateChanged((user) => {
-      setIsLoggedIn(!!user);
-    });
-    return unSubscribe;
-  }, []);
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setIsLoggedIn(true);
+            } else {
+                setIsLoggedIn(false);
+            }
+            setLoading(false);
+        });
 
+        // Cleanup the subscription on unmount
+        return () => unsubscribe();
+    }, []);
+
+    // Show a loading screen while checking auth status
+    if (loading) {
+        return (
+            <div style={{ color: "#fff", textAlign: "center", marginTop: "50px" }}>
+                Loading...
+            </div>
+        );
+    }
 
     return (
         <Router>
-          <AuthBackground>
-            <NavBar />
-            <Routes>
-                <Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/movie/:id" element={isLoggedIn ? <MovieDetail /> : <Navigate to="/login" />} />
-                <Route path="/favorites" element={isLoggedIn ? <FavoritesPage /> : <Navigate to="/login" />} />
-            </Routes>
+            <AuthBackground>
+                {isLoggedIn && <NavBar />}
+                <Routes>
+                    <Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/movie/:id" element={isLoggedIn ? <MovieDetail /> : <Navigate to="/login" />} />
+                    <Route path="/favorites" element={isLoggedIn ? <FavoritesPage /> : <Navigate to="/login" />} />
+                </Routes>
             </AuthBackground>
         </Router>
     );
 }
 
 export default App;
-
-
-{/* <Routes>
-<Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
-<Route path="/login" element={<Login />} />
-<Route path="/signup" element={<Signup />} />
-<Route path="/movie/:id" element={isLoggedIn ? <MovieDetail /> : <Navigate to="/login" />} />
-<Route path="/favorites" element={isLoggedIn ? <FavoritesPage /> : <Navigate to="/login" />} />
-</Routes> */}
